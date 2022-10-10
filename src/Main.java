cd43f80ae5cad6717e75322ee8d703eb18fcf2c7
kobanya <nagy.bela.budapest@gmail.com>
2022-10-10 16:00:18 +0200

    Kártya vagy Kártya nélküli fizetés megoldása. Paymant with or without card

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        read_in();
+        KartyasAr();
+    }
+
+//beolvasás
+
+    static void read_in() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adja meg a megrendelés árát: ");
+        MegrendelesAra = scanner.nextInt();
+
+        System.out.println("Adja meg a csomag súlyát: ");
+        CsomagSulya = scanner.nextInt();
+
+        System.out.println("Kártyával fizet ?  igen = 1 / nem = 2 :");
+        Kartya = scanner.nextInt();
+
+    }
+
+    static int MegrendelesAra;
+    static int CsomagSulya;
+    static int Kartya;
+    static int OsszegKatryaNelkul;
+    static int OsszegKartyaval;
+
+    // ha kártyával vásárolsz, 3 % kedvezményt kapsz
+    static int KartyasAr() {
+        if (Kartya == 1) {
+            OsszegKartyaval = MegrendelesAra+( MegrendelesAra * 3 / 100);
+            System.out.println("A termékek ára ha kártyával fizeted: " + OsszegKartyaval+"Euro");
+        } else if (Kartya == 2) {
+            //return OsszegKatryaNelkul;
+            System.out.println("A termékek ára ha készpénzben fizeted: " + MegrendelesAra+" Euro");
+        } else {
+            System.out.println("csak 0 és 1 választható");
+        }
+
+        return 0;
     }
 }
